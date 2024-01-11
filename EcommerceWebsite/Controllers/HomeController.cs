3972b767069bfe100f2c8b31b11a7e5b11358cfb
18130039 <80388171+18130039@users.noreply.github.com>
2024-01-11 22:55:21 +0700

    Add project files.

---
+++ EcommerceWebsite/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using EcommerceWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using X.PagedList;
+
+namespace EcommerceWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        QlbanVaLiContext db = new QlbanVaLiContext();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(int? page)
+        {
+            int pageSize = 8;
+            int pageNumber = page==null || page<0 ? 1 : page.Value;
+            var listProduct = db.TDanhMucSps.AsNoTracking().OrderBy(x =>x.TenSp);
+            PagedList<TDanhMucSp> listP = new PagedList<TDanhMucSp>(listProduct,pageNumber,pageSize);
+            return View(listP);
+        }
+        public IActionResult SanPhamTheoLoai(String maloai, int? page)
+        {
+            int pageSize = 8;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var listProduct = db.TDanhMucSps.AsNoTracking().Where(x=>x.MaLoai==maloai).OrderBy(x => x.TenSp);
+            PagedList<TDanhMucSp> listP = new PagedList<TDanhMucSp>(listProduct, pageNumber, pageSize);
+            ViewBag.maloai = maloai;
+            return View(listP);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EcommerceWebsite/Repository/ILoaiSpRepository.cs
@@ -0,0 +1,14 @@
+﻿using EcommerceWebsite.Models;
+
+namespace EcommerceWebsite.Repository
+{
+    public interface ILoaiSpRepository
+    {
+        TLoaiSp Add(TLoaiSp loaiSp);
+        TLoaiSp Update(TLoaiSp loaiSp);
+        TLoaiSp Delete(String maLoaiSp);
+        TLoaiSp GetLoaiSp(String maLoaiSp);
+        IEnumerable<TLoaiSp> GetAllLoaiSp();  
+
+    }
+}
