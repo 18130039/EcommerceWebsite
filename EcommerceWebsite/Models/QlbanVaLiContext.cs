2cc27797e34bafd5389f65d1883cdbbee0731f19
Nam An <201301962st.hcmuaf.edu.vn>
2024-01-13 18:41:51 +0700

    AdminLayout

--- EcommerceWebsite/Models/QlbanVaLiContext.cs
+++ EcommerceWebsite/Models/QlbanVaLiContext.cs
@@ -48,7 +48,8 @@ public partial class QlbanVaLiContext : DbContext
     public virtual DbSet<TUser> TUsers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Data Source=MSI\\SQLEXPRESS;Initial Catalog=QLBanVaLi;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-FKQTPA0;Initial Catalog=QLBanVaLi;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
