3fdaad6c1012955d2fdc5313068b91ba0b07d197
18130039 <80388171+18130039@users.noreply.github.com>
2024-01-16 17:05:46 +0700

    Add controller save ThoadonBan

---
+++ EcommerceWebsite/Controllers/CheckoutController.cs
@@ -0,0 +1,42 @@
+﻿using EcommerceWebsite.Infrastructure;
+using EcommerceWebsite.Models;
+using EcommerceWebsite.Models.Authentication;
+using EcommerceWebsite.Models.Cart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceWebsite.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly QlbanVaLiContext _context;
+
+        public CheckoutController(QlbanVaLiContext context)
+        {
+            _context = context;
+        }
+        /*        public IActionResult Checkout()
+                {
+                    return View("Checkout");
+                }*/
+        [Authentication]
+        public IActionResult Checkout()
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                var userId = HttpContext.Session.GetString("UserName");
+                string hdId = Guid.NewGuid().ToString("N").Substring(0, 25);
+                var tHoaDonBan = new THoaDonBan();
+                tHoaDonBan.MaHoaDon = hdId;
+                tHoaDonBan.NgayHoaDon = DateTime.Now;
+                tHoaDonBan.MaKhachHang = userId;
+                tHoaDonBan.MaNhanVien = "NV1";
+                _context.THoaDonBans.Add(tHoaDonBan);
+                _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
---
+++ EcommerceWebsite/Models/Checkout/CheckoutViewModel.cs
@@ -0,0 +1,24 @@
+﻿using EcommerceWebsite.Models.Cart;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+namespace EcommerceWebsite.Models.Checkout
+{
+    public class CheckoutViewModel
+    {
+        [Display(Name = "Ho va ten")]
+        public string Name { get; set; }
+
+        [Display(Name = "Dia chi chi tiet")]
+        public string Address { get; set; }
+
+        [Display(Name = "Quan/huyen")]
+        public string District { get; set; }
+
+        [Display(Name = "Tinh/Thanh pho")]
+        public string Province { get; set; }
+
+        [Display(Name = "Dien thoai")]
+        public string Phone { get; set; }
+/*        public List<CartLine> listCartItems { get; set; }*/
+    }
+}
